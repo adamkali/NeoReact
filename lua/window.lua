59b914a21f2afb8c24ff646690a2fd8db0d8c942
adamkali <adamkali@outlook.com>
2022-12-01 14:32:33 -0500

    hi

--- lua/window.lua
+++ lua/window.lua
@@ -7,6 +7,10 @@ local api = vim.api
 local buf, win
 local position = 0
 
+-- Set the window size to be 8 lines and 32 columns
+local width = 26
+local height = 6
+
 -- create an object to hold the name of the commands and the function to execute
 local template_mapping = {
 	['React Functional Component'] = code_templates.insert_edfc_template,
@@ -20,12 +24,22 @@ local function open_window()
 	api.nvim_buf_set_option(buf, 'bufhidden', 'wipe') -- set buffer options
 	local border_buf = api.nvim_create_buf(false, true) -- create new buffer for window
 
-	-- Set the window size to be 8 lines and 32 columns
-	local width = 32
-	local height = 8
+
 
 	-- Get the current position of the cursor
 	local row, col = unpack(api.nvim_win_get_cursor(0))
+
+	-- Set the position of the window three columns to the right of the cursor 
+	col = col + 3
+	row = row + 1
+
+	-- set the border width and height to be two more than the window
+	local border_width = width + 2
+	local border_height = height + 2
+
+	-- set the starting point of the border to be one less than the window
+	local border_start_row = row - 1
+	local border_start_col = col - 1
 
 	-- set some options
 	local opts = {
@@ -33,20 +47,21 @@ local function open_window()
 		relative = 'editor',
 		width = width,
 		height = height,
-		row = row + 2,
-		col = col + 2
+		row = row,
+		col = col
 	}
 
 	-- set border opts
 	local border_opts = {
 		style = 'minimal',
 		relative = 'editor',
-		width = width + 2,
-		height = height + 2,
-		row = row + 1,
-		col = col + 1
+		width = border_width,
+		height = border_height,
+		row = border_start_row,
+		col = border_start_col
 	}
 
+	-- create border with these characters ┌ ─ ┐ │ └ ┘
 	local border_lines =  { '╭' .. string.rep('─', width) .. '╮' }
 	local middle_line = '│' .. string.rep(' ', width) .. '│'
 	for i = 1, height do
@@ -60,27 +75,28 @@ local function open_window()
 
 	api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "' .. border_buf) -- close window when buffer is wiped out
 
-	-- add title
-	api.nvim_buf_set_lines(buf, 0, 1, false, {'NeoReact'})
-	api.nvim_buf_add_highlight(buf, -1, 'Title', 0, 0, -1)
+	-- Calculate the center of "React Templates"
+	local center = math.floor((width - #('React Templates')) / 2)
+
+	-- add a title to the window top center 
+	api.nvim_buf_set_lines(buf, 0, center, false, {'React Templates'})
 end
 
-local function update_window(direction)
-	api.nvim_buf_set_option(buf, 'modifiable', true)
+-- make a function to highlight the current line
+local function highlight_current_line()
+	api.nvim_buf_add_highlight(buf, -1, 'Visual', position, 0, -1)
+end
+
+-- make a function to move the cursor up and down
+local function move_cursor(direction)
+	api.nvim_buf_clear_namespace(buf, -1, 0, -1)
 	position = position + direction
-	if position < 0 then position = 0 end
-
-	-- fill the window with the template mapping keys
-	local lines = {}
-	for key, _ in pairs(template_mapping) do
-		table.insert(lines, key)
+	if position < 0 then
+		position = 0
+	elseif position > 4 then
+		position = 4
 	end
-
-	-- highlight the current line
-	api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-	api.nvim_buf_add_highlight(buf, -1, 'CursorLine', position, 0, -1)
-	api.nvim_buf_set_option(buf, 'modifiable', false)
-	
+	highlight_current_line()
 end
 
 local function close_window()
@@ -95,9 +111,9 @@ local function set_mapping()
 	-- q should quit
 	-- control+p should insert the highlighted template
 	local mappings = {
-		['q'] = close_window,
-		['j'] = function() update_window(1) end,
-		['k'] = function() update_window(-1) end,
+		['q'] = close_window(),
+		['j'] = move_cursor(1),
+		['k'] = move_cursor(-1),
 		['<C-p>'] = function()
 			-- insert the highlighted template
 			-- get the current line
@@ -111,18 +127,25 @@ local function set_mapping()
 		end
 	}
 
+	-- fill buf with the template names from the keys of the template_mapping object
+	local template_names = {}
+	for k, _ in pairs(template_mapping) do
+		table.insert(template_names, k)
+	end
+	api.nvim_buf_set_lines(buf, 1, -1, false, template_names)
+
 	for key, func in pairs(mappings) do
 		api.nvim_buf_set_keymap(buf, 'n', key, '<cmd>lua require"NeoReact".' .. func .. '()<CR>', {noremap = true, silent = true})
 	end
 
 	local other_chars = {
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-	  }
-	  for k,v in ipairs(other_chars) do
-		api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
-		api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
-		api.nvim_buf_set_keymap(buf, 'n',  '<c-'..v..'>', '', { nowait = true, noremap = true, silent = true })
-	  end
+	}
+	for k,v in ipairs(other_chars) do
+	api.nvim_buf_set_keymap(buf, 'n', v, '', { nowait = true, noremap = true, silent = true })
+	api.nvim_buf_set_keymap(buf, 'n', v:upper(), '', { nowait = true, noremap = true, silent = true })
+	api.nvim_buf_set_keymap(buf, 'n',  '<c-'..v..'>', '', { nowait = true, noremap = true, silent = true })
+	end
 end
 
 local function move_curser()
@@ -142,6 +165,11 @@ return {
 	NeoReactWindow = NeoReactWindow,
 	move_curser = move_curser,
 	close_window = close_window,
-	update_window = update_window,
-	refactor = code_templates.refactor_to_function_component
+	update_window = highlight_current_line,
+	refactor = code_templates.refactor_to_function_component,
+	-- Add the other functions here
+	insert_edfc_template = code_templates.insert_edfc_template,
+	insert_function_then_catch_template = code_templates.insert_function_then_catch_template,
+	insert_state_var_template = code_templates.insert_state_var_template,
+	insert_use_effect_template = code_templates.insert_use_effect_template
 }
